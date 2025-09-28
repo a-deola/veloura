d668bc9ef72638ef596203d8474bb030cb66a5ee
Adeola Samuel <meetprecious1403@gmail.com>
2025-09-28 10:00:24 +0100

    update nav

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,14 +1,47 @@
 import Image from "next/image";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "../ui/navigation-menu";
+import { Button } from "../ui/button";
+import Link from "next/link";
+
+const navLinks = [
+  { href: "/", label: "Home" },
+  { href: "/about", label: "About us" },
+  { href: "/services", label: "Our services" },
+  { href: "/gallery", label: "Gallery" },
+];
 export default function Navbar() {
   return (
-    <div>
+    <div className="flex justify-between items-center px-5 h-20">
       <Image
         src="/logo-black.png"
         alt="logo"
         width={150}
-        height={100}
+        height={10}
         priority
       />
+      <NavigationMenu viewport={false}>
+        <NavigationMenuList>
+          {navLinks.map(({ href, label }) => (
+            <NavigationMenuItem key={href}>
+              <NavigationMenuLink
+                asChild
+                className={navigationMenuTriggerStyle()}
+              >
+                <Link className="text-primary font-bold" href={href}>
+                  {label}
+                </Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+      <Button variant="outline">CONTACT US</Button>
     </div>
   );
 }
